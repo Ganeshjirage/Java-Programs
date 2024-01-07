2d175cf56a66cdcfe39855924c9b703fcf4dc2a2
Ganesh <Ganesh@host.docker.internal>
2024-01-07 23:49:30 +0530

    Added scanner class in LargestNo class

--- Programs/src/com/practice/questions/LargestNo.java
+++ Programs/src/com/practice/questions/LargestNo.java
@@ -1,18 +1,26 @@
 package com.practice.questions;
 
+import java.util.Scanner;
+
 public class LargestNo {
 
 	public static void main(String[] args) {
-		int[] array = new int[] { 10, 20, 50, 90, 70, 11 };
 
-		int max = array[0];
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the size of the array >> ");
+		int arr = scan.nextInt();
 
+		int[] array = new int[arr];
+
+		int maxNum = array[0];
+		System.out.println("Enter the elements of the array >>");
 		for (int i = 0; i < array.length; i++) {
-			if (array[i] > max) {
-				max = array[i];
+			array[i] = scan.nextInt();
+			if (array[i] > maxNum) {
+				maxNum = array[i];
 			}
 		}
-		System.out.println("Largest number is >> " + max);
+		System.out.println("Largest number is >> " + maxNum);
 
 	}
 
